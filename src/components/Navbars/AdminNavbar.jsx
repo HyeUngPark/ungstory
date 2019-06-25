a5594af19790c97ba53c03651dc4545be3bfaa0a
HyeUngPark <phu8460@gmail.com>
2019-06-25 18:46:17 +0900

    modal publishing working

--- src/components/Navbars/AdminNavbar.jsx
+++ src/components/Navbars/AdminNavbar.jsx
@@ -17,14 +17,17 @@ import {
   Container,
   Media
 } from "reactstrap";
+import PostWriteModal from '../../modals/PostWriteModal';
 
 class AdminNavbar extends React.Component {
   // closes the collapse
   noticeClick = (e, index) => {
     console.log("noticeClick >> ", index);
   };
-
   render() {
+    const modalData={
+      modal:true
+    }
     return (
       <>
         <Navbar className="navbar-top navbar-dark" expand="md" id="navbar-main">
@@ -110,21 +113,18 @@ class AdminNavbar extends React.Component {
                   <DropdownItem className="noti-title" header tag="div">
                     <h6 className="text-overflow m-0">Welcome!</h6>
                   </DropdownItem>
+                    <PostWriteModal/>
                   <DropdownItem to="/admin/user-profile" tag={Link}>
-                    <i className="ni ni-single-02" />
-                    <span>My profile</span>
-                  </DropdownItem>
-                  <DropdownItem to="/admin/user-profile" tag={Link}>
-                    <i className="ni ni-settings-gear-65" />
-                    <span>Settings</span>
-                  </DropdownItem>
-                  <DropdownItem to="/admin/user-profile" tag={Link}>
-                    <i className="ni ni-calendar-grid-58" />
-                    <span>Activity</span>
+                      <i className="ni ni-single-02" />
+                      <span>내 프로필</span>
                   </DropdownItem>
                   <DropdownItem to="/admin/user-profile" tag={Link}>
                     <i className="ni ni-support-16" />
                     <span>Support</span>
+                  </DropdownItem>
+                  <DropdownItem to="/admin/user-profile" tag={Link}>
+                    <i className="ni ni-settings-gear-65" />
+                    <span>설정</span>
                   </DropdownItem>
                   <DropdownItem divider />
                   <DropdownItem href="#pablo" onClick={e => e.preventDefault()}>
