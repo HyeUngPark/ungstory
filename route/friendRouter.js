367c55b820cd0d987844a0eb73a58f1855c6e71a
HyeUngPark <phu8460@gmail.com>
2019-11-01 22:55:36 +0900

    friend notice read temporary working

--- route/friendRouter.js
+++ route/friendRouter.js
@@ -153,6 +153,29 @@ router.post('/friendRequest',function(req,res){
 router.post('/frdReqList',function(req, res){
     var params = req.body;
     
+    // 0. 클릭 시점 친구 알림 0으로 변경
+    schema.updateMany({
+        wkCd : 'NOT'
+        ,wkDtCd : 'FRD'
+        ,fstWrDt:{"$lte":date.getDate()}
+    }
+    , { $set: {
+        lstWrDt : date.getDate()
+        ,'subSchema.readYn': true
+    }}
+    , function(uErr, uResult) {
+        if (uErr) {
+            console.log('error \n', uErr);
+            return res.status(500).send("친구 목록 클릭 시 알림 변경 실패 " + uErr)
+        }
+        if (uResult.n) {
+            console.log('★★★ 알람 0으로 변경 성공 ★★★');
+        }else{
+            console.log('★★★ 알람 변경 대상 없음 ★★★');
+        }
+    });
+
+
     // 1. 내 친구 목록 조회
     schema.aggregate([
         {$match : {
