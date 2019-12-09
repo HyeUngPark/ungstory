583c866046e0acb323591023b617b459b90fd97d
HyeUngPark <phu8460@gmail.com>
2019-12-09 20:35:05 +0900

    message list popup tempoary working

--- src/modals/msg/MsgList.jsx
+++ src/modals/msg/MsgList.jsx
@@ -8,6 +8,7 @@ import {Button,
         Row, 
         Col, 
         Dropdown,
+        UncontrolledDropdown,
         DropdownToggle,
         DropdownMenu,
         DropdownItem,
@@ -15,11 +16,12 @@ import {Button,
 
 import * as api from "utils/api";
 import * as popup from "utils/popup";
+import { withRouter } from 'react-router-dom';
 
 import { confirmAlert } from 'react-confirm-alert'; // Import
 import 'react-confirm-alert/src/react-confirm-alert.css'; // Import css
 
-export default class MsgList extends React.Component {
+class MsgList extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -27,6 +29,7 @@ export default class MsgList extends React.Component {
         ,searchName : ''
         ,searchList : []
         ,searchCd : false // 임시
+        ,msgMg : false
     };
     this.handleSubmit = this.handleSubmit.bind(this);
     // this.goPage = this.props.callbackFromParent;    
@@ -49,13 +52,19 @@ export default class MsgList extends React.Component {
 
   }
 
-//   goPages = (page)=>{
-//     if(page){
-//       this.toggle();
-//       this.goPage(page);
-//     }
-//   }
+  goPages = (page)=>{
+    if(page){
+      this.props.history.push(page);
+      this.toggle();
+    }
+  }
  
+  msgDelete = () =>{
+    // 1. 관리 아이콘, 새메시지 버튼 숨기기
+
+    // 2. 라디오버튼, 삭제하기 버튼 보여주기
+
+  }
 //   openPost =(e, pstPk) =>{
 //     this.imgClick.current.props.pstPk = pstPk;
 //     this.imgClick.current.toggle();
@@ -88,7 +97,44 @@ export default class MsgList extends React.Component {
           <Card className="bg-secondary shadow border-0">
             <h5 className="display-4">&nbsp;메시지</h5>
             <ModalBody>
-
+            <Row className="align-items-center" lg="12">
+              <Col lg="6">
+                {/* <FrdInfo callbackFromParent={this.goPage} frdName={post.usrName}/> */}
+              </Col>
+              <Col lg="6" className="text-right">
+                  <span>
+                  <button type="button" 
+                    className="btn btn-primary"
+                    onClick = {e=>{this.goPages('/user/msg-send')}}
+                  >
+                    새 메시지 작성
+                  </button>
+                    <UncontrolledDropdown
+                      isOpen={this.state.msgMg}
+                    >
+                        <DropdownToggle 
+                          className="pr-0" 
+                          onClick={e=>{this.setState({
+                              msgMg : !this.state.msgMg
+                            })
+                          }}
+                          nav>
+                          &nbsp;
+                          <i className="ni ni-settings-gear-65 form-control-cursor"/>
+                        </DropdownToggle>
+                        <DropdownMenu className="dropdown-menu-arrow " right>
+                          <DropdownItem
+                            className="form-control-cursor"
+                            onClick = {e=>{this.msgDelete()}}
+                          >
+                            <i className="ni ni-basket"/>
+                              메시지 삭제
+                          </DropdownItem>
+                        </DropdownMenu>
+                    </UncontrolledDropdown>
+                  </span>
+              </Col>
+            </Row>
             <Row className="justify-content-center"> 
               <Col>      
                 <Card className="card-profile shadow">
@@ -135,3 +181,6 @@ export default class MsgList extends React.Component {
     );
   }
 }
+
+
+export default withRouter(MsgList);
